998e8062425b4de584db16f465d652f4e758e22a
Jon Olav Eikenes <jonolav.eikenes@gmail.com>
2023-09-04 17:02:32 +0200

    Manual routing works. Indicate active page works

--- src/Router.svelte
+++ src/Router.svelte
@@ -1,54 +1,58 @@
 <!-- src/Router.svelte -->
 <script>
-  import { Router, Route, Link, navigate } from "svelte-routing";
   import Colours from "./pages/Colours.svelte";
   import Contrast from "./pages/Contrast.svelte";
+  import NotFound from "./pages/NotFound.svelte"; // A new 404 page component
   import { onMount, onDestroy } from "svelte";
-  import { writable } from "svelte/store";
 
-  // Use has
-  let currentHash = window.location.hash.slice(1); // Remove the '#' at the start
+  // Store what is current route / page
+  let currentRoute = "";
 
-  // Update currentHash when hash changes
-  function hashChangeHandler() {
-    currentHash = window.location.hash.slice(1);
-    if (currentHash === "/colour-tokens-overview/") {
-      window.location.hash = "/colour-tokens-overview/colours.html";
-    }
+  // When user navigates to a different hash route
+  function updateRoute() {
+    const hash =
+      window.location.hash.slice(1) || "/colour-tokens-overview/colours.html";
+    currentRoute = hash;
   }
 
-  //  Attach the event listener
+  //  Listen to changes in URL
   onMount(() => {
-    window.addEventListener("hashchange", hashChangeHandler, false);
-    if (window.location.hash === "") {
-      window.location.hash = "#/colour-tokens-overview/colours.html";
-    }
+    window.addEventListener("hashchange", updateRoute, false);
+    updateRoute(); // Initialize the route
   });
 
   // Clean up the event listener
   onDestroy(() => {
-    window.removeEventListener("hashchange", hashChangeHandler, false);
+    window.removeEventListener("hashchange", updateRoute, false);
   });
 </script>
 
-<!-- Nav bar -->
-<Router>
-  <nav class="p-8 s-bg-subtle">
-    <a
-      class="p-8 hover:s-bg-hover active:s-bg-selected"
-      href="#/colour-tokens-overview/colours.html">Colours in WARP</a>
-    <a
-      class="p-8 hover:s-bg-hover active:s-bg-selected"
-      href="#/colour-tokens-overview/contrast.html">Contrast</a>
-  </nav>
-  <Route path={currentHash}>
-    {#if currentHash === "/colour-tokens-overview/colours.html"}
-      <Colours />
-    {:else if currentHash === "/colour-tokens-overview/contrast.html"}
-      <Contrast />
-    {/if}
-  </Route>
-</Router>
+<nav class="p-8">
+  <a
+    class="p-8 rounded-4"
+    class:selected_page={currentRoute === '/colour-tokens-overview/colours.html'}
+    href="#/colour-tokens-overview/colours.html">Colours in WARP</a
+  >
+  <a
+  class="p-8 rounded-4"
+    class:selected_page={currentRoute === '/colour-tokens-overview/contrast.html'}
+    href="#/colour-tokens-overview/contrast.html">Contrast</a
+  >
+
+</nav>
+
+
+{#if currentRoute === "/colour-tokens-overview/colours.html"}
+  <Colours />
+{:else if currentRoute === "/colour-tokens-overview/contrast.html"}
+  <Contrast />
+{:else}
+  <NotFound />
+{/if}
 
 <style>
+  .selected_page {
+    background-color: var(--w-s-color-background-active);
+    font-weight: bold;
+  }
 </style>
---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,9 @@
+<!-- src/pages/NotFound.svelte -->
+<script>
+  // You can add some logic here if needed
+</script>
+
+<div>
+  <h1>404 - Not Found</h1>
+  <p>The page you are looking for doesn't exist.</p>
+</div>
